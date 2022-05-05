614c730aff4f4cb0164fee6d6bec7e82f5a4d381
enbyte <kieran@enbyte.dev>
2022-05-05 00:11:33 -0700

    Added GSON, started work on client history
    The idea now is to use regex to interpret/separate the file name and token to append them to links on their corresponding buttons stored in/drawn from a history file.

--- src/main/java/dev/enbyte/filingsaucerclient/UploadFile.java
+++ src/main/java/dev/enbyte/filingsaucerclient/UploadFile.java
@@ -1,5 +1,7 @@
 package dev.enbyte.filingsaucerclient;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -7,14 +9,18 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class UploadFile {
 
     String fileString = new String();
+
     public UploadFile(String fileString) throws IOException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpPost uploadFile = new HttpPost("http://000.000.000.000:0000/upload");
@@ -34,6 +40,12 @@ public class UploadFile {
         uploadFile.setEntity(multipart);
         CloseableHttpResponse response = httpClient.execute(uploadFile);
         HttpEntity responseEntity = response.getEntity();
+
+        // Response parsing.
+        String responseString = EntityUtils.toString(responseEntity, StandardCharsets.UTF_8);
+        Gson gson = new Gson();
+        JsonObject json = gson.fromJson(responseString, JsonObject.class);
+        System.out.println(json.get("message"));
     }
 
     public static void main(String fileString) throws IOException {
