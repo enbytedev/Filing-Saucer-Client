218189c1efa465e99eb6f12716158a93af4157c2
enbyte <kieran@enbyte.dev>
2022-05-16 20:53:00 -0700

    History works, application is fully functional.

--- src/main/java/dev/enbyte/filingsaucerclient/GUI.java
+++ src/main/java/dev/enbyte/filingsaucerclient/GUI.java
@@ -13,15 +13,13 @@ public class GUI implements ActionListener {
     JButton uploadButton = new JButton("Upload File");
     JButton openButton = new JButton("Open File");
     JButton deleteButton = new JButton("Delete File");
-    JButton historyButton = new JButton("History");
+    JButton historyButton = new JButton("History / Refresh");
     JPanel mainPanel = new JPanel(new GridLayout(8, 3));
     JPanel historyPanel = new JPanel(new GridLayout(8, 3));
 
     public void gui() {
         Timer timer = new Timer(20, this);
         timer.start();
-        historyView.removeAll();
-        historyView.repaint();
 
         // Setup GUI.
         uploadButton.addActionListener(this);
@@ -39,10 +37,18 @@ public class GUI implements ActionListener {
         mainView.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         mainView.pack();
         mainView.setVisible(true);
-        mainView.setSize(700, 500);
-        historyView.setSize(200, 500);
+        mainView.setSize(300, 250);
     }
 
+    public void ReadHistory() throws IOException {
+        HistoryList historyList = new HistoryList();
+        for (int i = 0; i < historyList.tokens.size(); i++) {
+            JTextArea fileNames = new JTextArea(historyList.fileName.get(i) + "Token: " + historyList.tokens.get(i));
+            fileNames.setLineWrap(true);
+            fileNames.setEditable(false);
+            historyPanel.add(fileNames);
+        }
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -74,11 +80,11 @@ public class GUI implements ActionListener {
         // * History
         if (e.getSource() == historyButton) {
             try {
-                HistoryList hl = new HistoryList();
-                hl.ReadHistory();
+                historyPanel.removeAll();
+                ReadHistory();
                 historyView.add(historyPanel);
                 historyView.pack();
-                historyView.repaint();
+                historyView.setSize(300, 500);
                 historyView.setVisible(true);
             } catch (IOException ioException) {
                 ioException.printStackTrace();
--- src/main/java/dev/enbyte/filingsaucerclient/HistoryList.java
+++ src/main/java/dev/enbyte/filingsaucerclient/HistoryList.java
@@ -14,23 +14,13 @@ public class HistoryList {
     ArrayList<Object> tokens = new ArrayList<>();
     ArrayList<Object> fileName = new ArrayList<>();
     public HistoryList() throws IOException {
+        assert listOfFiles != null;
         for (File listOfFile : listOfFiles) {
             if (listOfFile.isFile()) {
                 tokens.add(listOfFile.getName());
                 String content = Files.readString(Path.of(listOfFile.getPath()), StandardCharsets.UTF_8);
                 fileName.add(content);
             }
-        }
-    }
-
-    GUI gui = new GUI();
-    public void ReadHistory() throws IOException {
-        HistoryList historyList = new HistoryList();
-        for (int i = 0; i < historyList.tokens.size(); i++) {
-            JTextArea fileNames = new JTextArea(historyList.fileName.get(i) + "Token: " + historyList.tokens.get(i));
-            fileNames.setLineWrap(true);
-            fileNames.setEditable(false);
-            gui.historyPanel.add(fileNames);
         }
     }
 }
